5cbd4f35e6dc02aacb7520b901cd7e731d089c78
bancl <lyamwh@gmail.com>
2019-09-09 23:35:36 +0800

    137. Single Number II

---
+++ src/com/medium/bit/SingleNumberⅡ.java
@@ -0,0 +1,45 @@
+package com.easy.normal;
+
+/**
+ * {LeetCode No.137. Single Number II}
+ *
+ *
+ *
+ * 题解：
+ *      在给定的数组中找只出现一次的数字,重复的数字会有三个<p>
+ */
+public class RomanToInteger {
+
+    public int singleNumber(int[] nums) {
+        int x1 = 0, x2 = 0, mask = 0;
+         
+        for (int i : nums) {
+            x2 ^= x1 & i;
+            x1 ^= i;
+            mask = ~(x1 & x2);
+            x2 &= mask;
+            x1 &= mask;
+        }
+
+        return x1;  // Since p = 1, in binary form p = '01', then p1 = 1, so we should return x1. 
+                    // If p = 2, in binary form p = '10', then p2 = 1, and we should return x2.
+                    // Or alternatively we can simply return (x1 | x2).
+    }
+
+    public int singleNumberBool(int[] nums) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            int sum = 0;
+            for(int j = 0; j < nums.length; j++) {
+                if(((nums[j] >> i) & 1) == 1) {
+                    sum++;
+                    sum %= 3;
+                }
+            }
+            if(sum != 0) {
+                ans |= sum << i;
+            }
+        }
+        return ans;
+    }
+}
